ddfff94578ed713bdddbf709752953fb494ed188
NINE32 <83272735+NINE32@users.noreply.github.com>
2024-03-17 04:26:37 +0000

    Delete dev/components directory

--- dev/components/javascript.js
+++
@@ -1,9 +0,0 @@
-//Rotate circle icons on scroll
-window.onscroll = function () {
-    scrollRotate();
-};
-
-function scrollRotate() {
-    let image = document.getElementById("circle-icon");
-    image.style.transform = "rotate(" + window.pageYOffset/1 + "deg)";
-}
