c7b349dca4ab4e7109e9e22abb97a916818b6d4e
NINE32 <83272735+NINE32@users.noreply.github.com>
2024-03-18 07:37:34 +0000

    Add files via upload

---
+++ dev/locallinks/links_assets/javascript.js
@@ -0,0 +1,16 @@
+// Script to allow the expansion and collapsing of the different datacenter sites.
+var coll = document.getElementsByClassName("collapsible");
+    var i;
+    
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        } 
+      });
+    }
+// End of script to allow the expansion and collapsing of the different datacenter sites.
